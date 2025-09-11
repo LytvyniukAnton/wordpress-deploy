a5c0c86c92c2aa47ac70186a9c1dc82edfebee2d
Anton Lytvyniuk <balanr378@gmail.com>
2025-09-11 22:58:47 +0300

    Update ansible playbook for aapanel

--- terraform/security-group.tf
+++ terraform/security-group.tf
@@ -36,20 +36,6 @@ resource "aws_security_group" "nginx_security_group" {
     description = "Allow all outbound traffic"
   }
 
-  # ingress {
-  #   from_port   = 8443 # Port for access to Plesk
-  #   to_port     = 8443
-  #   protocol    = "tcp"
-  #   cidr_blocks = var.allowed_ips
-  # }
-
-  # ingress {
-  #   from_port   = 8447 # Port fot access to admin-panel
-  #   to_port     = 8447
-  #   protocol    = "tcp"
-  #   cidr_blocks = var.allowed_ips
-  # }
-
   ingress {
     from_port   = 8888 # Port for access to aaPanel
     to_port     = 8888
