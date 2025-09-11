fe485b0ac1ba8f69d9a3e3c2fb380605f7bf8b2f
Anton Lytvyniuk <balanr378@gmail.com>
2025-09-11 23:30:18 +0300

    Update ansible playbook for aapanel

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "nginx_server" {
-  ami           = "ami-0360c520857e5138f"
+  ami           = "ami-0bbdd8c17ed981ef9"
   instance_type = var.instance_type
   key_name      = var.key_name
   security_groups = [aws_security_group.nginx_security_group.name]
