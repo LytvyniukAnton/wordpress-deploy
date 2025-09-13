b044734a34725b3d2f86e214092bed1114931216
Anton Lytvyniuk <balanr378@gmail.com>
2025-09-13 05:59:14 +0300

    big fix + install aaPanel on a separate port

--- terraform/security-group.tf
+++ terraform/security-group.tf
@@ -37,27 +37,11 @@ resource "aws_security_group" "nginx_security_group" {
   }
 
   ingress {
-    from_port   = 8888 # Port for access to aaPanel
-    to_port     = 8888
+    from_port   = 8889
+    to_port     = 8889
     protocol    = "tcp"
     cidr_blocks = var.allowed_ips
   }
-
-  ingress {
-    from_port   = 8443 # Порт для доступу до aaPanel через Nginx
-    to_port     = 8443
-    protocol    = "tcp"
-    cidr_blocks = var.allowed_ips
-    description = "Allow HTTPS traffic for aaPanel"
-  }
-
-  # ingress {
-  #   from_port   = 8080 # Порт для доступу до aaPanel через Nginx
-  #   to_port     = 8080
-  #   protocol    = "tcp"
-  #   cidr_blocks = var.allowed_ips
-  #   description = "Allow HTTPS traffic for aaPanel"
-  # }
 
   tags = {
     Name = "Nginx-Security-Group"
